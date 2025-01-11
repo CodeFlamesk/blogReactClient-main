b9cefe3d3ae446b435fbb7bac95dd427b90000c2
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2025-01-11 02:03:08 +0100

    url update

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-export const _API_URL = "http://localhost:5000/api";
-export const _URL_SERVER = "http://localhost:5173/"
+/* export const _API_URL = "http://localhost:5000/api";
+export const _URL_SERVER = "http://localhost:5173/" */
+export const _API_URL = "https://blog-react-server-main.vercel.app/api";
+export const _URL_SERVER = "https://stmg.vercel.app/"
