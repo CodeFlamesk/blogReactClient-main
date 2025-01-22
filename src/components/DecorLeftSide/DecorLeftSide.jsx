a21e1965544f2a6e911d1fc8dadf071cdbaca67d
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2025-01-22 22:06:58 +0100

    add game over

--- src/components/DecorLeftSide/DecorLeftSide.jsx
+++ src/components/DecorLeftSide/DecorLeftSide.jsx
@@ -1,12 +1,16 @@
 import "./decor-left-side.scss"
 import soldier from "./img/soldier.png"
 import arrow from "./img/arrows.webp"
+import game from './img/game-img.webp'
+import over from './img/over-img.webp'
 const DecorLeftSide = () => {
 
     return (
         <div className="decor-page">
             {/*      <img src={arrow} alt="arrow" className="decor-page__arrow" /> */}
             <div className="decor-page__line"></div>
+            <img src={over} alt="over-img" className="decor-page__over-img" />
+            <img src={game} alt="game-img" className="decor-page__game-img" />
             <img src={soldier} alt="soldier" className="decor-page__soldier" />
             <img src={arrow} alt="arrow" className="decor-page__arrow-down" />
         </div>
