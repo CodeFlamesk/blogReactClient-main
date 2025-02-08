421b88614f816487bb482baca2ccca7d72d91e16
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2025-02-08 15:13:55 +0100

    main fix

--- src/components/DecorLeftSide/DecorLeftSide.jsx
+++ src/components/DecorLeftSide/DecorLeftSide.jsx
@@ -1,20 +1,47 @@
-import "./decor-left-side.scss"
-import soldier from "./img/soldier.png"
-import arrow from "./img/arrows.webp"
-import game from './img/game-img.webp'
-import over from './img/over-img.webp'
+import { useEffect } from "react";
+import "./decor-left-side.scss";
+import soldier from "./img/soldier.png";
+import arrow from "./img/arrows.webp";
+import game from './img/game-img.webp';
+import over from './img/over-img.webp';
+
 const DecorLeftSide = () => {
+    useEffect(() => {
+        const decorElements = document.querySelectorAll('.decor-page [class^="decor-page__"]');
+
+        const handleScroll = () => {
+            const viewportHeight = window.innerHeight;
+            const scrollY = window.scrollY;
+            const pageHeight = document.documentElement.scrollHeight;
+            const stopPosition = pageHeight - viewportHeight - 176;
+
+            decorElements.forEach(el => {
+                if (scrollY >= stopPosition) {
+                    el.style.position = 'absolute';
+                    el.style.bottom = '176px';
+                } else {
+                    el.style.position = 'fixed';
+                    el.style.bottom = el.dataset.bottom || 'auto';
+                }
+            });
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     return (
         <div className="decor-page">
-            {/*      <img src={arrow} alt="arrow" className="decor-page__arrow" /> */}
             <div className="decor-page__line"></div>
-            <img src={over} alt="over-img" className="decor-page__over-img" />
-            <img src={game} alt="game-img" className="decor-page__game-img" />
-            <img src={soldier} alt="soldier" className="decor-page__soldier" />
-            <img src={arrow} alt="arrow" className="decor-page__arrow-down" />
-        </div>
-    )
-}
+            <div className="decor-page__over-img" ><img src={over} alt="over-img" /></div>
+            <div className="decor-page__game-img" data-bottom="25%"> <img src={game} alt="game-img" /></div>
+            <img src={soldier} alt="soldier" className="decor-page__soldier" data-bottom="0px" />
+            <div className="decor-page__arrow-down" data-bottom="2%">
+                <img src={arrow} alt="arrow" />
+            </div>
 
-export default DecorLeftSide
+        </div>
+    );
+};
+
+export default DecorLeftSide;
