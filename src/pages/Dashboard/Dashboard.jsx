e92a872a68391e2e556a2cfc6aebdfaeed821e77
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2025-05-06 14:48:31 +0200

    redeploy

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,8 +4,7 @@ import { useSelector } from "react-redux";
 import DashboardPanel from "./DashboardPanel/DashboardPanel";
 
 import "./dashboard.scss"
-import DashboardAddNews from "./DashboardItems/DashboardItemAddNews/DashboardAddNews";
-import DashboardCategory from "./DashboardItems/DashboardCategory/DashboardCategory";
+
 
 const Dashboard = () => {
     const panel = useSelector(store => store.dashboard.panel)
@@ -15,8 +14,8 @@ const Dashboard = () => {
             <DashboardPanel />
             <div className="dashboard__right">
                 {panel === "dashboard"}
-                {panel === "news" && <DashboardAddNews />}
-                {panel === "category" && <DashboardCategory />}
+                {panel === "news"}
+                {panel === "category" >}
             </div>
         </div>
 
